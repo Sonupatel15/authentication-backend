a6916268a01e2342ec39d5203b05250b6723e1ec
Sonupatel A L <19358sonupatel@gmail.com>
2025-03-30 18:36:07 +0530

    Initial commit

---
+++ src/test/java/org/example/authenticationbackend/AuthenticationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authenticationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
